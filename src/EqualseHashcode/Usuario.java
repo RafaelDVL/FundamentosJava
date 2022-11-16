6771cf4efee005d10fd919fffe09320b1e7aa235
RafaelDvl <rafaelrodrigues.dsg3d@gmail.com>
2022-11-16 13:31:17 -0300

    Classes

---
+++ src/EqualseHashcode/Usuario.java
@@ -0,0 +1,21 @@
+package EqualseHashcode;
+
+public class Usuario {
+
+    String nome;
+    String email;
+
+    public boolean equals(Object obj) {
+
+        if(obj instanceof  Usuario) {
+            Usuario outro = (Usuario) obj;
+
+            boolean nomeIgual = outro.nome == this.nome;
+            boolean emailIgual = outro.email == this.email;
+
+            return nomeIgual && emailIgual;
+        } else {
+            return false;
+        }
+    }
+}
